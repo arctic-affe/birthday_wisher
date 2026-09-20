4f9360f5952c9f99ec03bd729d203facf6ca070d
Arctic Affe <arctic.affe@gmail.com>
2026-09-20 13:28:19 +0200

    First prototype designed

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,2 +1,16 @@
+import smtplib
+import json
 
+with open("credentials.json", "r") as file:
+    data = json.load(file)
 
+my_email = data["Email"]
+my_password = data["Password"]
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user = my_email, password = my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="a.radtschenko@yandex.ru",
+        msg="Subject:Very importatnt mail\n\nNa?!?!? Geht's schon? ")
